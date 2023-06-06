24f81d7700528233c49e08092f066db00051e5f8
Sophie Picard <sogautrelet@gmail.com>
2023-06-06 15:09:54 +0200

    modification of filterRecipes with the second algorithm

--- assets/brouillon.js
+++ assets/brouillon.js
@@ -1,44 +1,29 @@
 // Filtre les recettes en fonction d'une requête et de tags
 export function filterRecipes(recipes, query = { text: "", tags: [] }) {
-    const queryText = query.text;
-  
-    function matchesQuery(ingredient, query) {
-      return ingredient ? ingredient.includes(query) : false;
-    }
-  
-    const filtered = recipes.filter(recipe => {
-      const inTitle = recipe.name.toLowerCase().includes(queryText);
-      const inDescription = recipe.description.toLowerCase().includes(queryText);
-      const inIngredients = recipe.ingredients.some(ingredient => matchesQuery(ingredient, queryText));
-  
-      const tagsMatch = query.tags.every(tag =>
-        recipe.ingredients.some(ingredient => matchesQuery(ingredient, tag)) ||
-        recipe.appliance.includes(tag) ||
-        recipe.utensils.some(utensil => utensil.includes(tag))
-      );
-  
-      return (inTitle || inDescription || inIngredients) && tagsMatch;
-    });
-  
-    return filtered;
+  // Divise la requête en mots et les stocke dans queryTextWords
+  const queryTextWords = query.text.toLowerCase().split(" ");
+
+  // Vérifie si tous les mots de la requête sont présents dans les mots de l'élément donné
+  function matchesQuery(item, queryWords) {
+    return queryWords.every(queryWord => item.toLowerCase().includes(queryWord));
   }
 
+  const filtered = recipes.filter(recipe => {
+    const inTitle = matchesQuery(recipe.name, queryTextWords);
+    const inDescription = matchesQuery(recipe.description, queryTextWords);
+    const inIngredients = recipe.ingredients.some(ingredient =>
+      matchesQuery(ingredient, queryTextWords)
+    );
 
-// Frédéric Lépy
-// 10:38
-// const data = ""
+    const tagsMatch = query.tags.every(tag =>
+      recipe.ingredients.some(ingredient => matchesQuery(ingredient, [tag])) ||
+      recipe.appliance.includes(tag) ||
+      recipe.utensils.some(utensil => utensil.includes(tag))
+    );
 
-// const recipes = JSON.parse(data)
+    return (inTitle || inDescription || inIngredients) && tagsMatch;
+  });
 
-// const searches = ["coco", "chocolat", "pomme de terre", "poulet réunionais", "sdgdfhfgh", ""]
-// Frédéric Lépy
-// 10:39
-// function filterRecipes(search) {
-//   ...
-//   return filteredRecipes
-// }
+  return filtered;
+}
 
-// searches.forEach(search => filterRecipes(search))
-// Votre message
-
-// Envoyer
