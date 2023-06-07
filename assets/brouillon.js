d9977c06c5144826eaba0b1fd820069a01398d6a
Sophie Picard <sogautrelet@gmail.com>
2023-06-07 18:48:07 +0200

    modifications about notes

--- assets/brouillon.js
+++ assets/brouillon.js
@@ -1,33 +1,3 @@
-// filterRecipes avec ALGO 1
-
-// Filtre les recettes en fonction d'une requête et de tags
-export function filterRecipes(recipes, query = { text: "", tags: [] }) {
-  // Divise la requête en mots et les stocke dans queryTextWords
-  const queryTextWords = query.text.toLowerCase().split(" ");
-
-  // Vérifie si tous les mots de la requête sont présents dans les mots de l'élément donné
-  function matchesQuery(item, queryWords) {
-    return queryWords.every(queryWord => item.toLowerCase().includes(queryWord));
-  }
-
-  const filtered = recipes.filter(recipe => {
-    const inTitle = matchesQuery(recipe.name, queryTextWords);
-    const inDescription = matchesQuery(recipe.description, queryTextWords);
-    const inIngredients = recipe.ingredients.some(ingredient =>
-      matchesQuery(ingredient, queryTextWords)
-    );
-
-    const tagsMatch = query.tags.every(tag =>
-      recipe.ingredients.some(ingredient => matchesQuery(ingredient, [tag])) ||
-      recipe.appliance.includes(tag) ||
-      recipe.utensils.some(utensil => utensil.includes(tag))
-    );
-
-    return (inTitle || inDescription || inIngredients) && tagsMatch;
-  });
-
-  return filtered;
-}
 
 // JSBENCH.CH
 
