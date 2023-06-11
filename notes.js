48aa94009ea429bc93a7a7aaddac28f0f04eb9cb
Sophie Picard <sogautrelet@gmail.com>
2023-06-11 12:10:32 +0200

    last comments

--- notes.js
+++ notes.js
@@ -88,3 +88,65 @@ function filterRecipes2(search) {
 searches.forEach(search => {
   filterRecipes2(search);
 });
+
+// FONCTION FILTER POC/ALGO-2
+
+// La fonction prend en entrée un tableau de recettes et une requête de recherche, et renvoie un tableau de recettes filtrées qui correspondent à la requête.
+export function filterRecipes(recipes, query = { text: "", tags: [] }) {
+  
+  // Convertit le texte de la requête en minuscules et le divise en mots individuels.
+  const queryTextWords = query.text.toLowerCase().split(" ");
+
+  // Vérifie si tous les mots de la requête sont présents dans les mots de l'élément donné.
+  function matchesQuery(item, queryWords) {
+    // Convertit l'élément en minuscules pour une comparaison correcte.
+    const lowerCaseItem = item.toLowerCase();
+  
+    // Parcourt tous les mots de la requête.
+    for (let i = 0; i < queryWords.length; i++) {
+      // Si l'élément ne contient pas le mot de la requête actuelle, retourne false.
+      if (!lowerCaseItem.includes(queryWords[i])) {
+        return false;
+      }
+    }
+  
+    // Si tous les mots de la requête sont présents dans l'élément, retourne true.
+    return true;
+  }
+
+  // Création d'un tableau vide qui contiendra les recettes filtrées.
+  let filtered = [];
+
+  // Boucle à travers toutes les recettes.
+  for(let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
+    
+    // Vérifie si le titre ou la description de la recette contient tous les mots de la requête.
+    const inTitle = matchesQuery(recipe.name, queryTextWords);
+    const inDescription = matchesQuery(recipe.description, queryTextWords);
+    
+    // Vérifie si l'un des ingrédients de la recette contient tous les mots de la requête.
+    let inIngredients = false;
+    for(let j = 0; j < recipe.ingredients.length; j++) {
+      if(matchesQuery(recipe.ingredients[j], queryTextWords)) {
+        inIngredients = true;
+        break;
+      }
+    }
+    
+    // Vérifie si les tags sélectionnés correspondent aux ingrédients, à l'appareil ou aux ustensils de la recette.
+    const tagsMatch = query.tags.every(tag =>
+      recipe.ingredients.some(ingredient => matchesQuery(ingredient, [tag])) ||
+      recipe.appliance.includes(tag) ||
+      recipe.utensils.some(utensil => utensil.includes(tag))
+    );
+
+    // Si le titre, la description ou un ingrédient de la recette contient tous les mots de la requête, et que tous les tags correspondent, alors la recette est ajoutée au tableau filtré.
+    if ((inTitle || inDescription || inIngredients) && tagsMatch) {
+      filtered.push(recipe);
+    }
+  }
+
+  // Retourne le tableau de recettes filtrées.
+  return filtered;
+}
--- scripts/factories/recipeFactory.js
+++ scripts/factories/recipeFactory.js
@@ -1,49 +1,49 @@
 export function recipeFactory(data) {
   const { id, name, time, ingredients, description, appliance, utensils } = data;
-
+  
   // Création des éléments HTML
   function getRecipeCardDOM() {
     const card = document.createElement("article");
     card.classList.add("recipe-card");
-
+    
     const imgDiv = document.createElement("div");
     imgDiv.classList.add("recipe-img-box");
-
+    
     const img = document.createElement("img");
     img.classList.add("recipe-img");
     img.src = `assets/img/photos_recettes/${id}.jpg`;
     img.alt = name;  // Pour l'accessibilité, ajoutez un texte alternatif à l'image.
     imgDiv.appendChild(img);
-
+    
     const contentDiv = document.createElement("div");
     contentDiv.classList.add("recipe-content");
-
+    
     const presentationDiv = document.createElement("div");
     presentationDiv.classList.add("recipe-presentation");
-
+    
     const title = document.createElement("h2");
     title.classList.add("recipe-title");
     title.textContent = name;
-
+    
     const timeContent = document.createElement("div");
     timeContent.classList.add("recipe-time-content");
-
+    
     const timeIcon = document.createElement("span");
     timeIcon.classList.add("fa-regular", "fa-clock");
-
+    
     const timeElement = document.createElement("p");
     timeElement.classList.add("recipe-time");
     timeElement.textContent = `${time} min`;
-
+    
     const ingredientsContentDiv = document.createElement("div");
     ingredientsContentDiv.classList.add("recipe-ingredients-content");
-
+    
     const ingredientsDiv = document.createElement("div");
     ingredientsDiv.classList.add("recipe-ingredients");
-
+    
     const ingredientsList = document.createElement("ul");
     ingredientsList.classList.add("recipe-list");
-
+    
     // Parcours de chaque ingrédient de la recette
     ingredients.forEach(({ingredient, quantity = '', unit = ''}) => {
       const listItem = document.createElement("li");
@@ -53,33 +53,33 @@ export function recipeFactory(data) {
     
     const descriptionDiv = document.createElement("div");
     descriptionDiv.classList.add("recipe-description");
-
+    
     const descriptionElement = document.createElement("p");
     descriptionElement.textContent = description;
-
+    
     // Assemblage des éléments
     timeContent.appendChild(timeIcon);
     timeContent.appendChild(timeElement);
-
+    
     presentationDiv.appendChild(title);
     presentationDiv.appendChild(timeContent);
     
     ingredientsDiv.appendChild(ingredientsList);
-
+    
     descriptionDiv.appendChild(descriptionElement);
-
+    
     ingredientsContentDiv.appendChild(ingredientsDiv);
     ingredientsContentDiv.appendChild(descriptionDiv);
-
+    
     contentDiv.appendChild(presentationDiv);
     contentDiv.appendChild(ingredientsContentDiv);
-
+    
     card.appendChild(imgDiv);
     card.appendChild(contentDiv);
-
+    
     return card;
   }
-
+  
   // Retourne un objet contenant les propriétés de la recette
   // et la fonction qui crée la carte de recette
   return { id, name, time, ingredients, description, appliance, utensils, getRecipeCardDOM };
